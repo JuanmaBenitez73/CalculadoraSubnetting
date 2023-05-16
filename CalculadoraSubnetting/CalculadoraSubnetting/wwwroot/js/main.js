fe0f4565300ad1a8e7278a5683f1c0539ac5c6bd
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-16 13:44:42 +0200

    aaaa

--- CalculadoraSubnetting/CalculadoraSubnetting/wwwroot/js/main.js
+++ CalculadoraSubnetting/CalculadoraSubnetting/wwwroot/js/main.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    /*var hosts = document.getElementById("hosts-required").innerHTML;
+    var nSubnets = document.getElementById("n-subnets").getAttribute("@bind");
+
+    console.log(hosts);
+    console.log(nSubnets);*/
+    alert("Tony gai");
+}
+
 function incorrectRange() {
     alert("Alguno de los números está fuera del rango disponible");
 }
